b0a06847ccfc590be8b5cf1a055fcf23d20efbe2
Vikas <vikasvisking007@gmail.com>
2024-01-15 18:45:37 +0530

    implement mark as done

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,18 +1,37 @@
 <script setup>
+import { computed } from 'vue';
+import { UpdateToDo } from '@/services'
+
+const { task } = defineProps({
+    task: {
+        required: true
+    }
+})
+
+const emits = defineEmits(['update'])
+
+const getDate = computed(() => new Date(task.date).toLocaleDateString('en', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+}))
+
+const markAsDone = async () => {
+    if (!task.is_done) {
+        await UpdateToDo(task.id, { ...task, done: true })
+        emits('update')
+    }
+}
 </script>
 <template>
-    <div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card bg-base-100 shadow-xl">
         <div class="card-body">
-            <h2 class="card-title">To Do</h2>
-            <p>This is my First Task</p>
-            <div class="card-actions justify-between items-center">
-                <span>{{ new Date().toLocaleDateString('en', {
-                    weekday: 'long',
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                }) }}</span>
-                <button class="btn btn-primary">Mark as done</button>
+            <h2 class="card-title">{{ task.title }}</h2>
+            <p>{{ task.description }}</p>
+            <div class="card-actions justify-between items-center flex">
+                <span>{{ getDate }}</span>
+                <button class="btn btn-primary" v-if="!task.done" @click="markAsDone">Mark as done</button>
             </div>
         </div>
     </div>
--- src/services.js
+++ src/services.js
@@ -4,3 +4,7 @@ const BASE_URL = 'http://localhost:3000/'
 export const AddToDo = async (data) => {
     return await fetch(`${BASE_URL}to-do`, { method: 'post', body: JSON.stringify(data)}).then(res => res.json())
 }
+
+export const GetAllToDos = async () => await fetch(`${BASE_URL}to-do`).then(res => res.json())
+
+export const UpdateToDo = async (id, data) => await fetch(`${BASE_URL}to-do/${id}`, { method: 'put', body: JSON.stringify(data) }).then(res => res.json())
